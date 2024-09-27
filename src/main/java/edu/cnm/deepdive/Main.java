94c11fc4e283dc943938de1d9b00a891f8a8f32d
Matthew Page <mpage16@cnm.edu>
2024-09-27 10:12:36 -0600

    created Comparator sort to card in deck class.

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -1,5 +1,8 @@
 package edu.cnm.deepdive;
 
+import edu.cnm.deepdive.model.Deck;
+import edu.cnm.deepdive.service.ColorComparator;
+
 public class Main {
 
 
@@ -9,6 +12,8 @@ public class Main {
     deck.shuffle();
     System.out.println(deck);
     deck.sort();
+    System.out.println(deck);
+    deck.sort(new ColorComparator());
     System.out.println(deck);
 /*
     while (!deck.isEmpty()) {
