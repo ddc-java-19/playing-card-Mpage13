d1145e53b053924e89e450c251e5e906e24c14dc
Matthew Page <mpage16@cnm.edu>
2024-09-27 10:48:37 -0600

    outlined steps for card trick.

---
+++ src/main/java/edu/cnm/deepdive/Trick.java
@@ -0,0 +1,20 @@
+package edu.cnm.deepdive;
+
+public class Trick {
+
+  public static void main(String[] args) {
+    // TODO create and shuffle a deck of cards.
+    // TODO create two empty lists - one for the black pile, and one for the red pile.
+    // TODO while the deck is not empty, deal cards, two at a time:
+    //  if first card is black, put the second into the black pile;
+    //  otherwise put the second card into the red pile.
+    // TODO Pick a random number between 0 and the size of the smaller of the two piles (inclusive).
+    // TODO Swap that picked number of cards between the two piles.
+    // TODO Count the number of black cards in the black pule and the number of cards in the red pile.
+    // TODO Display the contents of each pile in color order, followed by the corresponding count.
+
+
+
+  }
+
+}
